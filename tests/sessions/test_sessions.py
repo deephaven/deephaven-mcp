906185c8e06b9344f34be47750b25ec60783982f
Chip Kent <5250374+chipkent@users.noreply.github.com>
2025-06-13 14:46:03 -0600

    precommit

--- tests/sessions/test_sessions.py
+++ tests/sessions/test_sessions.py
@@ -15,10 +15,11 @@ from pydeephaven import Session
 from deephaven_mcp import config
 from deephaven_mcp.config._community_session import redact_community_session_config
 from deephaven_mcp.sessions import _sessions
-from deephaven_mcp.sessions._queries import get_dh_versions
 from deephaven_mcp.sessions._errors import SessionCreationError
-from deephaven_mcp.sessions._sessions import SessionManager
 from deephaven_mcp.sessions._lifecycle.community import create_session
+from deephaven_mcp.sessions._queries import get_dh_versions
+from deephaven_mcp.sessions._sessions import SessionManager
+
 
 # --- Fixtures and helpers ---
 @pytest.fixture
